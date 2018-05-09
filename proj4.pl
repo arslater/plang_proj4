2ec242aab4ed23087b90fd7184adbed0814bb0d0
anita <slater.anita@gmail.com>
2018-05-09 15:51:50 -0400

    [proj4] output is now a little bit nicer, gave variables longer names to make output nicer.

--- proj4.pl
+++ proj4.pl
@@ -8,15 +8,15 @@ title(dancing).
 title(happy).
 title(magnet).
 
-ttime(t15).
-ttime(t31).
-ttime(t38).
-ttime(t55).
+ttime(fifteen).
+ttime(thiry-one).
+ttime(thirty-eight).
+ttime(fifty-five).
 
-view(mil4).
-view(mil5).
-view(mil6).
-view(mil7).
+view(four).
+view(five).
+view(six).
+view(seven).
 
 solve :-
 	
@@ -37,43 +37,43 @@ solve :-
 				[stacy,StacyTitle, StacyTime, StacyView]],
 
 	%Rule 1:
-	((member([_,happy,_  ,mil6],Quads));
-    (member([_,_    ,t15,mil6],Quads)) ),
-	not(member([_,happy,t15,_],Quads)),
+	((member([_,happy,_  ,six],Quads));
+    (member([_,_    ,fifteen,six],Quads)) ),
+	not(member([_,happy,fifteen,_],Quads)),
 
 	%Rule 2:
-	((member([_,magnet,_  ,mil7],Quads));
-	 (member([_,magnet,t31,_   ],Quads)) ),
-	not(member([_,_,t31,mil7],Quads)),	
+	((member([_,magnet,_  ,seven],Quads));
+	 (member([_,magnet,thirty-one,_   ],Quads)) ),
+	not(member([_,_,thirty-one,seven],Quads)),	
 
 	%Rule 3:
-	member([stacy,_,t15,_],Quads),
+	member([stacy,_,fifteen,_],Quads),
 	
 	%Rule 4:
-	not(member([_,baby,t38,_],Quads)),
+	not(member([_,baby,thirty-eight,_],Quads)),
 	
 	%Rule 5:
-	((member([carl,_,_,mil6],Quads),
-	  member([_,dancing,_,mil4],Quads));
-	 (member([carl,_,_,mil7],Quads),
-	  member([_,dancing,_,mil5],Quads)) ),
+	((member([carl,_,_,six],Quads),
+	  member([_,dancing,_,four],Quads));
+	 (member([carl,_,_,seven],Quads),
+	  member([_,dancing,_,five],Quads)) ),
 
 	%Rule 6:
-	member([bill,_,_,mil4],Quads),
+	member([bill,_,_,four],Quads),
 	
 	%Rule 7:
-	not(member([inez,happy,t31,_],Quads)),
+	not(member([inez,happy,thirty-eight,_],Quads)),
 
 	%Rule 8:
 	((member([carl,magnet,_,_],Quads),
-	  member([_,_,t15,mil6],Quads));
-	 (member([carl,_,_,mil6],Quads),
-     member([_,magnet,t15,_],Quads)) ),
-	 not(member([carl,_,t15,_],Quads)),
-	 not(member([_,magnet,_,mil6],Quads)),
+	  member([_,_,fifteen,six],Quads));
+	 (member([carl,_,_,six],Quads),
+     member([_,magnet,fifteen,_],Quads)) ),
+	 not(member([carl,_,fifteen,_],Quads)),
+	 not(member([_,magnet,_,six],Quads)),
 
 	tell(bill, BillTitle,  BillTime,  BillView),
-   tell(carl, CarlTitle,  CarlTime,  CarlView),
+	tell(carl, CarlTitle,  CarlTime,  CarlView),
 	tell(inez, InezTitle,  InezTime,  InezView),
 	tell(stacy,StacyTitle, StacyTime, StacyView).
 
@@ -82,6 +82,7 @@ all_different([_|T]) :- all_different(T).
 all_different([_]).
 
 tell(W,X,Y,Z) :-
-	write(W),write(' is the author of the '),write(X),write(' video.'),
-   write(Y),write('seconds and '), write(Z), write('million views'),nl.
+	write(W), write('\'s video, '),write('"'),write(X),
+   write('"'),write(', has '),write(Z),write(' million views '),
+   write('and is '),write(Y),write(' seconds long.'), nl.
 
